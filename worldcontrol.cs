bfdff93194f7fe90bedd189a173451afdc093f9b
Nahta Realnaame <47120653+mentalburden@users.noreply.github.com>
2020-07-09 18:45:50 -0700

    Update worldcontrol.cs

--- worldcontrol.cs
+++ worldcontrol.cs
@@ -6,30 +6,47 @@ using System;
 using System.Linq;
 using System.Security.Cryptography;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WorldControl : MonoBehaviour
 {
     public int boundarysize;
+    public int epochpage;
     public List<particletype> pt;
     public GameObject[,] particleobjects;
     public GameObject testobj;
+    public RawImage uiimage;
+    public Texture2D testimage;
+    Color[] testpixels;
+    
 
     void Start()
     {
+
+        testimage = new Texture2D(boundarysize, boundarysize, TextureFormat.ARGB32, false);
+        testpixels = new Color[boundarysize * boundarysize];        
         pt = new List<particletype>();
         particleobjects = new GameObject[boundarysize, boundarysize];
         for (int i=0, x = 0; x <= boundarysize; x++)
         {
             for (int z = 0; z <= boundarysize; z++)
             {
-                int coinflip = getrand(1, 2);
+                int coinflip = getrand(0, 3);
+                if (coinflip == 0)
+                {
+                    pt.Add(new particletype(i, x, z, 0, true, 0));
+                }
                 if (coinflip == 1)
                 {
-                    pt.Add(new particletype(i, x, z, 0, true));
+                    pt.Add(new particletype(i, x, z, 1, false, 0));
                 }
                 if (coinflip == 2)
                 {
-                    pt.Add(new particletype(i, x, z, 0, false));
+                    pt.Add(new particletype(i, x, z, 2, false, 0));
+                }
+                if (coinflip == 3)
+                {
+                    pt.Add(new particletype(i, x, z, 3, false, 0));
                 }
                 i++;
                 Debug.Log(i.ToString());
@@ -40,33 +57,6 @@ public class WorldControl : MonoBehaviour
     public particletype GetItem(int x, int z)
     {
         return pt.Find(chonk => chonk.px == x && chonk.pz == z);
-    }
-
-    public void applychild(int x, int z)
-    {
-        try
-        {
-            Vector3 thispos = new Vector3(x, 0, z);
-            GameObject myobj = Instantiate(testobj, thispos, Quaternion.identity);
-            particleobjects[x, z] = myobj;
-        }
-        catch
-        {
-
-        }
-    }
-
-    public void destroychild(int x, int z)
-    {
-        try
-        {
-            Destroy(particleobjects[x, z].gameObject);
-            Debug.Log("destroyed object at " + x.ToString() + " " + z.ToString());
-        }
-        catch
-        {
-
-        }
     }    
 
     public static int getrand(int min, int max)
@@ -89,13 +79,21 @@ public class WorldControl : MonoBehaviour
         particletype below = GetItem(x, z - 1);
         particletype belowleft = GetItem(x - 1, z - 1);
         particletype belowright = GetItem(x + 1, z - 1);
+        if (me.page >= epochpage)
+        {
+            me.ptype = 0;
+            me.pstate = false;
+            me.page -= epochpage;
+            return;
+        }
         if (me != null && below != null && belowleft != null && belowright != null)
         {
             if (below.pstate == false)
             {
                 me.pz -= 1;
                 below.pz += 1;
-                //below.pstate = false;
+                me.page -= epochpage;
+                //below.pstate = true;
                 return;
             }
             else if (belowleft.pstate == false && belowright.pstate == true && below.pstate == true)
@@ -104,6 +102,7 @@ public class WorldControl : MonoBehaviour
                 me.px -= 1;
                 belowleft.pz += 1;
                 belowleft.px += 1;
+                me.page -= epochpage;
                 return;
             }
             else if (belowleft.pstate == true && belowright.pstate == false && below.pstate == true)
@@ -112,36 +111,68 @@ public class WorldControl : MonoBehaviour
                 me.px += 1;
                 belowright.pz += 1;
                 belowright.px -= 1;
+                me.page -= epochpage;
+                return;
+            }
+            else if (belowleft.pstate == true && belowright.pstate == true && below.pstate == true)
+            {
+                me.page = me.page + 1;
+                //me.pz -= 1;
+                //me.px += 1;
+                //belowright.pz += 1;
+                //belowright.px -= 1;
                 return;
             }
         }
-        else if (x == 0)
-        {
-            me.pstate = false;
-            return;
-        }
+    }
+
+    public void restartscene()
+    {
+        SceneManager.LoadScene(0);
     }
 
     void Update()
     {
         //calc
-        for (int x = 0; x < boundarysize; x++)
+        for (int i = 0, x = 0; x < boundarysize; x++)
         {
             for (int z = 0; z < boundarysize; z++)
-            {                
+            {
+                int thistype = GetItem(x, z).ptype;
                 //run the neighbor operation and set the coords
-                runsolidkernel(x, z);
+
                 //run the draw or instantiate afterwards
-                if (GetItem(x,z).pstate == true)
+                if (thistype == 0)
                 {
-                    applychild(x, z);
+                    testpixels[i] = Color.blue;
+                    //testpixels[(int)x * testimage.width + (int)z] = Color.white;
+                    //applychild(x, z);
                 }
-                else if (GetItem(x, z).pstate == false)
+                else if (thistype == 1)
                 {
-                    destroychild(x, z);
+                    testpixels[i] = Color.white;
+                    //testpixels[(int)x * testimage.width + (int)z] = Color.black;
+                    //applychild(x, z);
                 }
+                else if (thistype == 2)
+                {
+                    testpixels[i] = Color.white;
+                    //testpixels[(int)x * testimage.width + (int)z] = Color.green;
+                    //applychild(x, z);
+                }
+                else if (thistype == 3)
+                {
+                    testpixels[i] = Color.white;
+                    //testpixels[(int)x * testimage.width + (int)z] = Color.red;
+                    //applychild(x, z);
+                }
+                i++;
+                runsolidkernel(x, z);
             }
-        }
+        }        
+        testimage.SetPixels(testpixels);
+        testimage.Apply();
+        uiimage.texture = testimage;
     }
 }
 
